eb6c6903c4d16ae5cf3bfba8b5e7acabbfa89662
Aashi Malhotra <aashimalhotra@Aashis-MacBook-Pro.local>
2021-04-09 02:55:17 -0400

    initial setup for map view

--- squirrel_tracker/views.py
+++ squirrel_tracker/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'squirrel_tracker/index.html',{})
+
+
+def map(request):
+    return render(request, 'squirrel_tracker/map.html')
