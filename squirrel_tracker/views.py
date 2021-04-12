c1792b1dd2803ff0ecc5045432b271f36a2dd959
Aashi Malhotra <aashimalhotra@Aashis-MacBook-Pro.local>
2021-04-11 23:46:00 -0400

    Creates form view to display for a particular squirrel id

--- squirrel_tracker/views.py
+++ squirrel_tracker/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from squirrel_tracker.models import Squirrel
+from squirrel_tracker.forms import SquirrelForm
 
 def index(request):
     return render(request, 'squirrel_tracker/index.html',{})
-
 
 def map(request):
     squirrel_data = Squirrel.objects.all()[:100]
@@ -13,7 +13,9 @@ def sightings(request):
     return render(request, 'squirrel_tracker/sightings.html', {})
 
 def update_squirrel(request, squirrel_id):
-    return render(request, 'squirrel_tracker/update.html', {})
+    squirrel_data = Squirrel.objects.get(UniqueSquirrelID=squirrel_id)
+    form_data = SquirrelForm(instance=squirrel_data)
+    return render(request, 'squirrel_tracker/update.html', {'form_data': form_data})
 
 def add_sighting(request):
     return render(request, 'squirrel_tracker/add.html', {})
