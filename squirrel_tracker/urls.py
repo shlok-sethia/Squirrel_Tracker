b483894162ff7c1869e19d63ce74f3c1850d2675
Aashi Malhotra <aashimalhotra@Aashis-MacBook-Pro.local>
2021-04-11 23:15:33 -0400

    adds form for updating all squirrel fields

--- squirrel_tracker/urls.py
+++ squirrel_tracker/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('map/', views.map, name='map'),
     path('sightings/', views.sightings, name='sightings'),
-    path('sightings/<squirrel_id>', views.update_squirrel, name='update'),
+    path('sightings/<str:squirrel_id>', views.update_squirrel, name='update'),
     path('sightings/add', views.add_sighting, name='add'),
     path('sightings/stats', views.get_stats, name='stats')
 ]
